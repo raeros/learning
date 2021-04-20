390d7a69abfc4564b1df65e81900eb5385802c0c
Rodrigo Andrade <raeros42@gmail.com>
2021-04-20 14:23:56 -0300

    Added tests for the project

--- go/my-first-go-application/greetings/greetings.go
+++ go/my-first-go-application/greetings/greetings.go
@@ -16,6 +16,7 @@ func Hello(name string) (string, error) {
 	// if a name was received, return a value that embeds the name
 	// in a greeting message.
 	message := fmt.Sprintf(randomFormat(), name)
+
 	return message, nil
 }
 
